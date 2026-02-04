1c2b030b047759a6d0f995dbb1a10e5b72c817ac
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2026-02-04 20:53:46 +0530

    Added VariableDataType folder

---
+++ VariableDataType/JavaBasic.java
@@ -0,0 +1,10 @@
+public class JavaBasic {
+    public static void main(String args[]) {
+        System.out.println("Hello Vaibhav Dubey !");
+        System.out.println("Hello Kaise ho !");
+        System.out.println("Hello Kya Haal !");
+        System.out.println("Hello Vibhu !");
+    }
+}
+
+//Boilierplate code for Java programming Language.
