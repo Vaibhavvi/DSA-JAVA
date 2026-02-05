7e9ff825d688f079ad2c50f20beaae71f74cfcc2
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2026-02-05 20:15:39 +0530

    Added JavaIntermediate and updated JavaBasic Today Learn about Variable and DataType and Taking Input and many more concept

--- VariableDataType/JavaBasic.java
+++ VariableDataType/JavaBasic.java
@@ -1,9 +1,37 @@
 public class JavaBasic {
     public static void main(String args[]) {
+
+        // Print Hello World and other statements .....
         System.out.println("Hello Vaibhav Dubey !");
         System.out.println("Hello Kaise ho !");
         System.out.println("Hello Kya Haal !");
         System.out.println("Hello Vibhu !");
+
+        // Vaiables Declaration and Initialization
+        int c = 10;
+        int d = 20;
+
+        System.out.println("The sum of c and d is : " + (c+d));
+
+
+        // Data Types in Java
+
+        // long and double are 8bytes in size
+        long myLongNum = 100000000000L; // size 8 bytes
+        double myDoubleNum = 19.99d; // size 8 bytes
+        int myNum = 5; // size 4 bytes
+        float myFloatNum = 6.99f; // size 4 bytes
+        char myLetter = 'D';  // size 2 bytes
+        boolean myBool = true;  // size 1 bit
+        String myText = "Hello World"; // size depends on the string
+        System.out.println("Integer : "+ myNum + "\n" + " Float :"+ myFloatNum + "\n" + " Character :"+ myLetter + "\n" + " Boolean :"+ myBool + "\n" + " String :"+ myText + "\n" + " Long :"+ myLongNum + "\n" + " Double :"+ myDoubleNum);
+
+        // Sum of two Numbers 
+
+        int a = 50;
+        int b = 20;
+        int sum = a+b;
+        System.out.println("The sum of a and b is : "+ sum);
     }
 }
 
---
+++ VariableDataType/JavaIntermediate.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class JavaIntermediate {
+    public static void main(String args[]) {
+
+        // Taking User Input in Java
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello "+ name + " !");
+
+        // Sum of two numbers using user input
+
+        System.out.println("Enter the first number : ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number : ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of "+ num1 + " and " + num2 + " is : " + sum);
+
+
+        // Circle Area Calculation
+
+        System.out.println("Enter the radius of the circle : ");
+        double radius = sc.nextDouble();
+        double area = 3.14 * radius * radius;
+        System.out.println("The area of the circle is : " + area);
+    }
+}
