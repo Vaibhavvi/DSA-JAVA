122d0fb08cba98a191f9213b136d09091e839ec7
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2026-03-14 19:43:23 +0530

    Start Function Basic (DSA-JAVA)

---
+++ Function/BasicFun.java
@@ -0,0 +1,9 @@
+public class BasicFun {
+
+    public static void printHello() {
+        System.out.println("Hello Vaibhav");
+    }
+    public static void main(String[] args){
+        printHello();
+    }  
+}
