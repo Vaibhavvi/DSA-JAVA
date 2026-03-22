d1e15222212e2225090bad21678afc3e1be3e65b
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2026-03-22 23:09:14 +0530

    Completed Functions With Practice Question (DSA-JAVA)

--- Function/AdvanceFunc.java
+++ Function/AdvanceFunc.java
@@ -10,10 +10,35 @@ public class AdvanceFunc {
         return a+b+c;
     }
 
+    // Check Prime Number or Not with Optimized code
+    public static boolean isPrime(int n){
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Print all prime range
+    public static void PrimeinRange(int n){
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){
+                System.out.println(i+ " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // Function Call
 
         System.out.println(Sum(10, 20));
         System.out.println(Sum(10, 20 , 30));
+
+        // Prime Function call
+        System.out.println(isPrime(11));
+
+        //Call Print prime range 
+        PrimeinRange(20);
     }
 }
