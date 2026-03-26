ce6efdfb8a8577cb6f6225a608bd879e7923c87f
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2026-03-26 17:13:02 +0530

    Started arrays and solved pattern question (DSA-JAVA)

---
+++ Arrays/BasicArray.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class BasicArray {
+    public static void main(String[] args) {
+        int[] marks = new int[50];
+        marks[1] = 85;
+        marks[2] = 55;
+        marks[0] = 35;
+        System.out.println(marks[1]);
+
+    }
+}
--- Pattern/HardPattern.java
+++ Pattern/HardPattern.java
@@ -10,7 +10,7 @@ public class HardPattern {
             for(int j = 1; j <= totalCols; j++) {
 
                 // Border condition
-                if(i == 1 || i == totalRows || j == 1 || j == totalCols) {
+                if(i == 1 ||  i == totalRows || j == 1 || j == totalCols) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
@@ -21,7 +21,47 @@ public class HardPattern {
         }
     }
 
+    //02 - Inverted and Rotated Half Pyramid
+
+    public static void HalfPyramid(int n){
+        //outer Loop
+        for (int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    /* 
+
+    //03 Inverted Half Paramid pattern with number
+    public static void HalfPyramidNumber(int n) {
+        //outer loop
+        for (int i=1; i<=n; i++){
+            //number print
+            for (int j=1; j>=i; j++){
+                System.out.print(i);
+            }
+
+            for (int j=1; j<)
+            System.out.println();
+        }
+    }
+
+    */
+
     public static void main(String[] args) {
         hollow_rectangle(4, 5);
+
+        // 02 Method Call 
+
+        HalfPyramid(4);
+
     }
 }
